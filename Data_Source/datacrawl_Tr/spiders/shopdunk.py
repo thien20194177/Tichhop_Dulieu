c9767969567ce240552cf8240ea6e793ae7c19c5
Trang-Thuy <82387290+Trang-Thuy@users.noreply.github.com>
2023-06-14 22:53:40 +0700

    upload full data shopdunk.json
    Data Shopdunk (updated, 103 items)

--- Data_Source/datacrawl_Tr/spiders/shopdunk.py
+++ Data_Source/datacrawl_Tr/spiders/shopdunk.py
@@ -2,117 +2,58 @@ import scrapy
 
 class BookspiderSpider(scrapy.Spider):
     name = "shopdunkspider"
-   
     #allowed_domains = [ 'shopdunk']
-    
-
     start_urls=[]
+
     def start_requests(self):
       start_urls = ['https://shopdunk.com/iphone']
       for url in start_urls:
         yield scrapy.Request(url, callback=self.parse)
 
-
     def parse(self, response):
-
         phones = response.css('div .item-box .product-item')
-
-        for phone in phones:
         
+        for phone in phones:
             relative_url = phone.css('h2 a ::attr(href)').get()
             phone_url = 'https://shopdunk.com' + relative_url        
             yield response.follow (phone_url, callback=self.newparse)         
         
+    def newparse(self, response):     
+        phone_url_list = response.css('div.page-body div.product-essential div.overview div.attributes ul.option-list li a ::attr(href)').extract()
+        x = len (phone_url_list)
         
-    def newparse(self, response): 
-    
-        x = len(response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li"))
-        y = len(response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li"))
-
-        if x < y:
-            k = y
-        else: 
-            k = x
-        
-        a = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[1]/a").get()
-        
-        if a is None:
-            yield response.follow (response.xpath("/html/head/link[3]").attrib['href'], callback=self.parse_phone_page)
+        if x == 0:
+            phone_url = response.xpath("/html/head/meta[11]").attrib['content']
+            yield response.follow (phone_url, callback=self.parse_phone_page) 
         
         else:
-
-            if len(response.xpath("//div[@class='attributes']/dl/dd")) > 3:
-                m = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[1]").get()
-                if m is not None:
-                    a = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[1]/a").attrib['href']
-                    if k >=2 :                     
-                        b = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[2]/a").attrib['href']
-                    if k>= 3:
-                        c = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[3]/a").attrib['href']
-                    if k >=4:
-                        d = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[4]/a").attrib['href']
-                else:
-                    a = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[1]/a").attrib['href']
-                    if k >=2:
-                        b = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[2]/a").attrib['href']
-                    if k >= 3:    
-                        c = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[3]/a").attrib['href']
-                    if k >=4:    
-                        d = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[4]/a").attrib['href']
-            else:
-                    a = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[1]/a").attrib['href']
-                    if k >=2:
-                        b = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[2]/a").attrib['href']
-                    if k >= 3:    
-                        c = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[3]/a").attrib['href']
-                    if k >=4:    
-                        d = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[1]/ul/li[4]/a").attrib['href']
-
-                
-            
-            
-
-            for i in range (0, k, 1):   
-            
-                    if i == 0:
-                        phone_url = 'https://shopdunk.com' + a
-                    yield response.follow (phone_url, callback=self.parse_phone_page)
-                    
-                    if i == 1:
-                        yield response.follow ('https://shopdunk.com' + b , callback=self.parse_phone_page)
-                    
-                    if i == 2:
-                        yield response.follow ('https://shopdunk.com' + c , callback=self.parse_phone_page)
-                    
-                    if i == 3:
-                        yield response.follow ('https://shopdunk.com'  +d,  callback=self.parse_phone_page)      
+            for i in range (0, x):
+                phone_url = 'https://shopdunk.com' + phone_url_list[i]
+                yield response.follow (phone_url, callback=self.parse_phone_page)   
 
         
     def parse_phone_page(self,response):  
+        c = len(response.css('div.page-body div.product-essential div.overview div.attributes ul.attribute-squares li label span ::attr(title)').extract())
+        color_list = response.css('div.page-body div.product-essential div.overview div.attributes ul.attribute-squares li label span ::attr(title)').extract()
+
+        price = response.css('div .prices .product-price span::text').get()
+        price = price.replace(" ", "")
+        price = price.replace("\n", "")
+
+        memory = response.css('div.page-body div.product-essential div.overview div.attributes ul.option-list li a label.checked-attr ::text').get()
+        if memory is None:
+            memory = response.css('div.page-body div.product-essential div.overview div.attributes ul.option-list li label ::text').get()
         
-        k = len(response.xpath("//div[@class='attributes']/dl/dd"))
-               
-        if len(response.xpath("//div[@class='attributes']/dl/dd")) > 3:
-            m = response.xpath("//form/div[2]/div[1]/div[4]/div[4]/dl/dd[3]/ul/li[1]").get()
-            if m is not None:
-                x = len(response.xpath("//div[@class='attributes']/dl/dd")) - 1
-            else: 
-                x = len(response.xpath("//div[@class='attributes']/dl/dd")) - 3
-        else: 
-                x = len(response.xpath("//div[@class='attributes']/dl/dd")) - 1
-       
-    
-        yield{
-                'url': response.xpath("//html/head/link[3]").attrib['href'],
-                'name':response.css('h1 .main-name ::text').get(),
-                'img_url': response.css('div .product-essential .gallery .picture a::attr(href)').get(),
-                'price': response.css('div .prices .product-price span::text').get(),
-                'memory': response.xpath("//div[@class='attributes']/dl/dt[{0}]/span[1]/text()".format(str(x))).get(),
-                'color1':response.xpath("///div[4]/dl/dd[{0}]/ul/li[1]/label/span".format(str(k))).attrib['title'],
-                'color2':response.xpath("///div[4]/dl/dd[{0}]/ul/li[2]/label/span".format(str(k))).attrib['title'],
-                #'color3':response.xpath("///div[4]/dl/dd[{0}]/ul/li[3]/label/span".format(str(k))).attrib['title'],
-            
-            }
+        for i in range (0, c):
+            color = color_list[i]    
+            yield{
+                    'url': response.xpath("//html/head/link[3]").attrib['href'],
+                    'name':response.css('h1 .main-name ::text').get(),
+                    'img_url': response.css('div .product-essential .gallery .picture a::attr(href)').get(),
+                    'price': price.replace("\r", ""),
+                    'memory': memory,
+                    'color1': color
+                }
                 
 
        
